3668d2b15775b8b27eec9b2d0c58dbfaf4a27417
Nava Teja <mushamnavam9530@gmail.com>
2024-03-03 15:12:16 +0530

    Updated according to PEP8

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def combinations(string):
-    combis = []
+"""This module returns all the possible combinations of all the lengths of a given string"""
+
+def get_combinations(string: str):
+    """Generates the combinations one by one"""
+
     for num in range(1, 2 ** len(string)):
         combiner = bin(num).replace('0b', '')
         while len(combiner) < len(string):
@@ -8,6 +11,8 @@ def combinations(string):
         for index, state in enumerate(combiner):
             if state == '1':
                 combi += string[index]
-        combis.append(combi)
-    return combis
-    
+        yield combi
+
+if __name__ == '__main__':
+    sample = input('Enter any string: ')
+    print(list(get_combinations(sample)))
